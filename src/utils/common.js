a49ad1b9ddd626fcfa3fb94a716d02017d83a57b
jyaymie <jamiesujiepark@gmail.com>
2022-08-07 16:54:33 -0700

    Add a utils folder that holds common functions and enums

---
+++ src/utils/common.js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { BASE_API_URL, LOCAL_STORAGE_KEY } from './enums';
+
+export function setAuthLocalStorage(token) {
+	localStorage.setItem(LOCAL_STORAGE_KEY, token);
+}
+
+export function getAuthLocalStorage() {
+	return localStorage.getItem(LOCAL_STORAGE_KEY);
+}
+
+// Remove all query params from the current path.
+export function getPathFromUrl(url) {
+	return url.split(/[?#]/)[0];
+}
+
+// https://onur.dev/writing/useFetch-react-hook
+
+// Fetching data is now done through the useAuthFetch custom hook, which pulls
+// the auth token from local storage. With local storage, the token persists on
+// page refresh. (Using a global context happens to clear the token on page
+// refresh.)
+
+// This custom hook also handles loading states, error messages, and data
+// refreshing.
+
+export function useAuthFetch(path) {
+	const errorMessage =
+		'Hm, something went wrong. Please try again or contact jamieparkemail@gmail.com.';
+	const token = getAuthLocalStorage();
+
+	const [data, setData] = useState([]);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [url, setUrl] = useState(`${BASE_API_URL}/${path}/`);
+
+	// ======================================================================= GET
+	const fetchItems = () => {
+		setError('');
+		setLoading(true);
+
+		axios
+			.get(url, {
+				// A token is required to access any API route.
+				headers: {
+					Authorization: `Token ${token}`,
+				},
+			})
+			.then((res) => {
+				setLoading(false);
+				setData(res.data);
+			})
+			.catch((err) => {
+				setLoading(false);
+				console.log('Something went wrong...', err);
+				setError(errorMessage);
+			});
+	};
+
+	// ==================================================================== DELETE
+	const deleteItem = (item) => {
+		const noQueryUrl = getPathFromUrl(url);
+		setError('');
+		setLoading(true);
+
+		axios
+			.delete(`${noQueryUrl}${item.id}/`, {
+				headers: {
+					Authorization: `Token ${token}`,
+				},
+			})
+			.then(() => {
+				setLoading(false);
+				fetchItems();
+			})
+			.catch((err) => {
+				setLoading(false);
+				console.log(err);
+				setError(errorMessage);
+			});
+	};
+
+	// ======================================================================= PUT
+	const updateItem = (item) => {
+		const noQueryUrl = getPathFromUrl(url);
+		setError('');
+		setLoading(true);
+
+		axios
+			.put(`${noQueryUrl}${item.id}/`, item, {
+				headers: {
+					Authorization: `Token ${token}`,
+				},
+			})
+			.then(() => {
+				fetchItems();
+			})
+			.catch((err) => {
+				setLoading(false);
+				console.log(err);
+				setError(errorMessage);
+			});
+	};
+
+	// ================================================================= useEffect
+	useEffect(() => {
+		fetchItems();
+	}, [url, token]);
+
+	return { data, loading, error, deleteItem, setUrl, updateItem };
+}
--- src/utils/enums.js
+++ src/utils/enums.js
@@ -1,4 +1,10 @@
+// Running 'npm start' will create a dev environment. (See package.json.)
 export const IS_DEV = process.env.REACT_APP_ENV === 'dev' ? true : false;
+
+// If you're in a dev environment, the local url will be used.
+// Otherwise, the production url will be used.
 export const BASE_API_URL = IS_DEV
-	? 'http://localhost:8000'
+	? 'http://localhost:8000/api'
 	: 'https://siftora.herokuapp.com/api';
+
+export const LOCAL_STORAGE_KEY = 'siftoraApp';
