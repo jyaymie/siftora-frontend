21ad73055e7dd5a8624a088c1d88d9fc1f7e2008
jyaymie <104411072+jyaymie@users.noreply.github.com>
2023-02-07 20:34:37 -0800

    Update base url

--- src/utils/enums.js
+++ src/utils/enums.js
@@ -4,7 +4,7 @@ export const IS_DEV = process.env.REACT_APP_ENV === 'dev' ? true : false;
 // In a dev env, the local url is used. If not, the production url is used.
 export const BASE_API_URL = IS_DEV
 	? 'http://localhost:8000/api'
-	: 'https://siftora.herokuapp.com/api';
+	: 'https://web-production-40f4.up.railway.app/api';
 
 export const LOCAL_STORAGE_KEY = 'siftoraApp';
 
